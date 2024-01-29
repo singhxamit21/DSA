a1e2450217b664f18f244c95f4ca421af6ead8a9
Amit Singh <amitk.singh@aurionpro.com>
2024-01-29 15:48:08 +0530

    First Commit

---
+++ array/reverseArray.js
@@ -0,0 +1,17 @@
+function reverseArray(arr){
+    let start = 0;
+    let end = arr.length - 1;
+    
+    while(start < end){
+        let temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+
+        start++;
+        end--;
+    }
+    return arr;
+}
+
+console.log(reverseArray([1,2,3,4,5]))
+console.log(reverseArray(["a","c","m","b","y"]))
