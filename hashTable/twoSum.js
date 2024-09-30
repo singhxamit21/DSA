6348762bd8ee5f90282ee602d7118b2692bbc0d0
Amit Singh <amitk.singh@aurionpro.com>
2024-09-30 14:57:47 +0530

    hotfix

--- hashTable/twoSum.js
+++ hashTable/twoSum.js
@@ -1,16 +1,13 @@
 function twoSum(nums, target) {
-  const numMap = {};
-
-  for (let i = 0; i < nums.length; i++) {
-    const complement = target - nums[i];
-    if (complement in numMap && numMap[complement] !== i) {
-      return [numMap[complement], i];
+  let numObj = {};
+    for (let i = 0; i < nums.length; i++) {
+      let complement = target - nums[i];
+      if (numObj[complement] !== undefined) {
+        return [numObj[complement], i];
+      }
+      numObj[nums[i]] = i;
     }
-    numMap[nums[i]] = i;
   }
-
-  return [];
-}
 
 const nums = [2, 7, 11, 15];
 const target = 9;
