3e93ff88d1f728e91f9a171e9ca677091bb9fcd2
Amit Singh <amitk.singh@aurionpro.com>
2024-10-24 12:56:11 +0530

    closures real world example

--- basicConcept/closures.js
+++ basicConcept/closures.js
@@ -142,4 +142,41 @@ isSubscribed();
 isSubscribed();
 
 
+//Closures Real World Example
+// https://medium.com/@techsuneel99/how-i-have-mastered-closures-in-react-a6b121095a92
+
+// import React, { useState } from 'react';
+
+// const Counter = () => {
+//   const [count, setCount] = useState(0);
+
+//   // State is declared inside the closure
+//   const createCounter = () => {
+//     const increment = () => {
+//       setCount((prevCount) => prevCount + 1);
+//       console.log('Incremented! Current count:', count + 1);
+//     };
+
+//     const decrement = () => {
+//       setCount((prevCount) => prevCount - 1);
+//       console.log('Decremented! Current count:', count - 1);
+//     };
+
+//     return { increment, decrement };
+//   };
+
+//   const counterInstance = createCounter();
+
+//   return (
+//     <div>
+//       <p>Count: {count}</p>
+//       <button onClick={counterInstance.increment}>Increment</button>
+//       <button onClick={counterInstance.decrement}>Decrement</button>
+//     </div>
+//   );
+// };
+
+// export default Counter;
+
+
 
