a0a8dc8234737b4e2d6408326e20ae138a1a8251
Amit Singh <amitk.singh@aurionpro.com>
2024-11-25 12:19:56 +0530

    longest common prefix

--- String/longestCommonPrefix.js
+++ String/longestCommonPrefix.js
@@ -14,5 +14,17 @@ function longestCommonPrefix(strs) {
     }
 };
 
+function longestCommonPrefix2(words){
+    // check border cases size 1 array and empty first word)
+    if (!words[0] || words.length ==  1) return words[0] || "";
+    let i = 0;
+    // while all words have the same character at position i, increment i
+    while(words[0][i] && words.every(w => w[i] === words[0][i]))
+      i++;
+    
+    // prefix is the substring from the beginning to the last successfully checked i
+    return words[0].slice(0, i);
+  }
+
 console.log(longestCommonPrefix(["flower", "flow", "flight"]))
 console.log(longestCommonPrefix(["dog", "racecar", "car"]))
