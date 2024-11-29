52475b6f6c16372dfee246f5d5c50a6195e69ea9
Amit Singh <amitk.singh@aurionpro.com>
2024-11-29 12:03:06 +0530

    min and max in array

--- array/maxiMinimumNumberArray.js
+++ array/maxiMinimumNumberArray.js
@@ -1,55 +1,26 @@
+
+function findInduildMinMax(arr) {
+    let minValue = Math.min(...arr);
+    let maxValue = Math.max(...arr);
+    return { min: minValue, max: maxValue }
+}
+
 function findMinMax(arr) {
-    if (arr.length === 0) {
-        return { min: undefined, max: undefined };
+    let minValue = Infinity;
+    let maxValue = -Infinity;
+    for (let item of arr) {
+
+        // Find minimum value
+        if (item < minValue)
+            minValue = item;
+
+        // Find maximum value
+        if (item > maxValue)
+            maxValue = item;
     }
-    let min, max;
-    // Initialize min and max with the first two elements
-    if (arr[0] < arr[1]) {
-        min = arr[0];
-        max = arr[1];
-    } else {
-        min = arr[1];
-        max = arr[0];
-    }
-
-    // Process the rest of the array in pairs
-    //i += 2 means that after each iteration, the value of i will be increased by 
-    for (let i = 2; i < arr.length - 1; i += 2) {
-        let currentMin, currentMax;
-
-        // Compare current pair of elements
-        if (arr[i] < arr[i + 1]) {
-            currentMin = arr[i];
-            currentMax = arr[i + 1];
-        } else {
-            currentMin = arr[i + 1];
-            currentMax = arr[i];
-        }
-
-        // Update overall min and max
-        if (currentMin < min) {
-            min = currentMin;
-        }
-
-        if (currentMax > max) {
-            max = currentMax;
-        }
-    }
-
-    // If the array has an odd length, handle the last element separately
-    if (arr.length % 2 === 1) {
-        let lastElement = arr[arr.length - 1];
-
-        if (lastElement < min) {
-            min = lastElement;
-        } else if (lastElement > max) {
-            max = lastElement;
-        }
-    }
-
-    return { min, max };
+    return { min: minValue, max: maxValue }
 }
 
 // Example usage
-let result = findMinMax([1,2,3,4,2,6,10000,8,9,2]);
+let result = findMinMax([1, 2, 3, 4, 2, 6, 10000, 8, 9, 2]);
 console.log(result); // Output: { min: 1, max: 9 }
