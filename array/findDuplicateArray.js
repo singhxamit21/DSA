6e8145ee6f494245d1b1eb4665a44aeb4aa1ddba
Amit Singh <amitk.singh@aurionpro.com>
2025-01-08 12:03:54 +0530

    some new question in duplicate of Array

--- array/findDuplicateArray.js
+++ array/findDuplicateArray.js
@@ -26,3 +26,32 @@ function countofDuplicateElementArray(arr){
 }
 
 console.log(countofDuplicateElementArray([1, 2, 3, 4, 5, 2, 7, 8, 3, 4,10,10,6,10]))
+
+
+//unique in Array object
+const Obj = [
+    { name: "prayag", Age: 23 },
+    { name: "prayag", Age: 23 },
+    { name: "Amit", Age: 24 }
+];
+
+const uniqueLetters = [...new Set(Obj.map(a => JSON.stringify(a)))].map(e => JSON.parse(e));
+
+console.log(uniqueLetters);
+
+// ------->>>>>>>>>>>>>>>>>>>>
+var array2 = [
+    { "name": "Joe", "age": 17 },
+    { "name": "Bob", "age": 17 },
+    { "name": "Carl", "age": 35 }
+];
+
+function getDistinctValues(array, key) {
+    return [...new Set(array.map(item => item[key]))];
+}
+
+var distinctAges = getDistinctValues(array2, "age");
+console.log(distinctAges); // [17, 35]
+
+var distinctNames = getDistinctValues(array2, "name");
+console.log(distinctNames); // ["Joe", "Bob", "Carl"]
