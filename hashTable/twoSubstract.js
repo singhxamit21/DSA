4f1b81932d821abf12d6c534aa8223e47c4c61a2
singhxamit21 <singhxamit21@gmail.com>
2025-03-16 17:00:39 +0530

    some additional changhe

--- hashTable/twoSubstract.js
+++ hashTable/twoSubstract.js
@@ -1,24 +1,25 @@
 function twoSubtract(nums, target) {
-    let numObj = {};
-    for (let i = 0; i < nums.length; i++) {
-      let complement = nums[i] - target; // Check for nums[i] - x = target (i.e., complement = nums[i] - target)
-      if (numObj[complement] !== undefined) {
-        return [numObj[complement], i];
-      }
+  let numObj = {};
   
-      // Also check for the reverse case: complement = nums[i] + target, which gives x - nums[i] = target
-      complement = nums[i] + target;
-      if (numObj[complement] !== undefined) {
-        return [numObj[complement], i];
+  for (let i = 0; i < nums.length; i++) {
+      let complement1 = nums[i] - target; // nums[i] - x = target
+      let complement2 = nums[i] + target; // x - nums[i] = target
+
+      if (complement1 in numObj) {
+          return [numObj[complement1], i];
       }
-  
+
+      if (complement2 in numObj) {
+          return [numObj[complement2], i];
+      }
+
       numObj[nums[i]] = i;
-    }
-    return null; // Return null if no valid pair is found
   }
   
-  const nums = [2, 7, 11, 15];
-  const target = 5;
-  const result = twoSubtract(nums, target);
-  console.log(result); // [0, 1] because 7 - 2 = 5
-  
+  return null; // Return null if no valid pair is found
+}
+
+// Example usage:
+const nums = [2, 7, 11, 15];
+const target = 5;
+console.log(twoSubtract(nums, target)); // Output: [0, 1] because 7 - 2 = 5
--- hashTable/twoSum.js
+++ hashTable/twoSum.js
@@ -1,15 +1,21 @@
 function twoSum(nums, target) {
   let numObj = {};
-    for (let i = 0; i < nums.length; i++) {
-      let complement = target - nums[i];
-      if (numObj[complement] !== undefined) {
-        return [numObj[complement], i];
-      }
-      numObj[nums[i]] = i;
+  
+  for (let i = 0; i < nums.length; i++) {
+    let complement = target - nums[i];
+    
+    if (complement in numObj) {
+      return [numObj[complement], i];
     }
+    
+    numObj[nums[i]] = i;
   }
+  
+  return null; // Explicitly return null if no pair is found
+}
 
+// Example usage:
 const nums = [2, 7, 11, 15];
 const target = 9;
 const result = twoSum(nums, target);
-console.log(result); // [0, 1]
+console.log(result); // Output: [0, 1] because 2 + 7 = 9
