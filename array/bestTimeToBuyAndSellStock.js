6997419e1f0fc040d6e0ea9b20dc4d1430002259
unknown <singhxamit21@gmail.com>
2025-06-13 13:32:44 +0530

    best time to buy

--- array/bestTimeToBuyAndSellStock.js
+++ array/bestTimeToBuyAndSellStock.js
@@ -2,33 +2,33 @@
 //so you have to tell on which day you will buy and on which day you will sell so that you can have a maximum profit
 
 
-const maxProfit = (prices) => {
-    let minPrice = prices[0]; // Assume the first day is the cheapest to buy
-    let maxProfit = 0;
-  
-    for (let i = 1; i < prices.length; i++) {
-      const currentPrice = prices[i];
-      // console.log(currentPrice);
-  
-      // Update minimum price if a lower price is found
-      minPrice = Math.min(minPrice, currentPrice);
-      // console.log(minPrice);
-  
-      // Calculate potential profit for selling today
-      const potentialProfit = currentPrice - minPrice;
-      // console.log(potentialProfit);
-  
-      // Update maxProfit if a higher profit is found
-      maxProfit = Math.max(maxProfit, potentialProfit);
-      // console.log(maxProfit);
-    }
-  
-    return maxProfit;
-  };
-  
-  const prices = [7, 1, 5, 3, 6, 4]; //list of stock prices for each day
-  const profit = maxProfit(prices);
-  console.log("Maximum profit:", profit);
+const maxProfitWithDays = (prices) => {
+  let minPrice = prices[0];
+  let maxProfit = 0;
+  let buyDay = 0;
+  let sellDay = 0;
+  let tempBuyDay = 0;
 
-console.log(maxProfit([7,1,5,3,6,4]))    //output 5
-console.log(maxProfit([7,6,4,3,1]))      //output 0
+  for (let i = 1; i < prices.length; i++) {
+    if (prices[i] < minPrice) {
+      minPrice = prices[i];
+      tempBuyDay = i;
+    }
+
+    const potentialProfit = prices[i] - minPrice;
+    if (potentialProfit > maxProfit) {
+      maxProfit = potentialProfit;
+      buyDay = tempBuyDay;
+      sellDay = i;
+    }
+  }
+
+  return {
+    maxProfit,
+    buyDay,
+    sellDay
+  };
+};
+
+const result = maxProfitWithDays([7, 1, 5, 3, 6, 4]);
+console.log(`Max profit: ${result.maxProfit}, Buy on day ${result.buyDay}, Sell on day ${result.sellDay}`);
