b884bf4396ee7f4be7030bae061c404785e20d91
Amit Singh <amitk.singh@aurionpro.com>
2025-07-10 10:46:07 +0530

    fix

--- String/longestCommonPrefix.js
+++ String/longestCommonPrefix.js
@@ -22,7 +22,7 @@ function longestCommonPrefix(strs) {
   for (let i = 1; i < strs.length; i++) {
       for (let j = 0; j < prefix.length; j++) {
           if (prefix[j] !== strs[i][j]) {
-              prefix = prefix.substring(0, j);
+              prefix = prefix.slice(0, j);
               break;
           }
       }
